4f5cf48a47189f9d445ae48166a237aeba59ebd2
John Moore <jmoore@zedstar.org>
2018-06-01 12:39:07 +0100

    dockerize the build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# compile code
+echo "compiling..."
+eval `opam config env`
+cd src && jbuilder build server.exe
+cp example-server-key ../
+cp example-token-key ../
+cp ./_build/default/server.exe ../
+echo "done compiling"
